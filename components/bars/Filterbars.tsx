c5db170428f6373439e262152e091d999c05a655
unknown <codemonga@gmail.com>
2023-09-12 15:14:07 +0100

    error checks

--- components/bars/Filterbars.tsx
+++ components/bars/Filterbars.tsx
@@ -17,7 +17,6 @@ const Filterbars: FC<Status> = ({ open, close}) => {
     return (
       <aside className={open ? 'fixed w-full top-0 left-0 bg-primary/30 h-screen z-50' : 'hidden'} id='filterbar' onClick={handleClose}>
         <div className='absolute w-64 px-10 top-0 left-0 bg-primary h-full pt-28 space-y-10'>
-          <Filterform />
           <button type='button' className='absolute top-0 right-0 mr-3' onClick={close}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
               <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
--- components/bars/Menubar.tsx
+++ components/bars/Menubar.tsx
@@ -4,7 +4,6 @@ import Filterform from '../forms/Filter'
 const Menubar = () => {
     return (
       <aside className='pt-6 lg:pt-20 hidden lg:block lg:col-span-1 lg:pl-20 space-y-10'>
-          <Filterform />
       </aside>
     )
 }
